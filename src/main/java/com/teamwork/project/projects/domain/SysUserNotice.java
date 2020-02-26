0f6bc1759e214637349cbdcb5647422d23720a9a
xinglinlin <1375493958@qq.com>
2020-02-26 23:00:42 +0800

    xll

---
+++ src/main/java/com/teamwork/project/projects/domain/SysUserNotice.java
@@ -0,0 +1,45 @@
+package com.teamwork.project.projects.domain;
+
+import java.io.Serializable;
+
+/**
+ * (SysUserNotice)实体类
+ *
+ * @author makejava
+ * @since 2020-02-24 21:02:10
+ */
+public class SysUserNotice implements Serializable {
+    private static final long serialVersionUID = 970743379110256945L;
+    
+    private Long id;
+    
+    private Long userId;
+    
+    private Long noticeId;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getNoticeId() {
+        return noticeId;
+    }
+
+    public void setNoticeId(Long noticeId) {
+        this.noticeId = noticeId;
+    }
+
+}
